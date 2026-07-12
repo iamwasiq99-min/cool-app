86eca4014731a10600277f7e9b7cf1caa1eb7226
user1 <user1@example.com>
2026-07-12 12:02:46 -0700

    add amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Amplify } from 'aws-amplify'
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(() =>{
+    console.log('Amplify COnfigure')
+    console.log(Amplify.getConfig())
+  })
 
   return (
     <>
